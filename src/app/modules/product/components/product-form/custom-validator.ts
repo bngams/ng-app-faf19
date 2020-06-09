a91e09500cc8f7a4b4e0c3555e2541a405d57a7a
bngams <n.boris@devidia.net>
2020-06-09 18:01:49 +0200

    remove custom validator DI

--- src/app/modules/product/components/product-form/custom-validator.ts
+++ src/app/modules/product/components/product-form/custom-validator.ts
@@ -1,9 +1,6 @@
 import { Validator, ValidationErrors, AbstractControl } from '@angular/forms';
 import { Injectable } from '@angular/core';
 
-@Injectable({
-    providedIn: 'root'
-})
 export class CustomValidator implements Validator {
     validate(control: AbstractControl): ValidationErrors {
         console.log('in validate');
